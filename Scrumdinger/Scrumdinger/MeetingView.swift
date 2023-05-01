095018e2dbf9694a9d3740fa507a59117c8f80dc
DavideVS <dvenseg@posgrado.upv.es>
2023-05-01 17:22:23 +0200

    Create a proyect Swift
    Create proyect and Compose views.

---
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Davide Venier Segovia on 1/5/23.
+//
+
+/*
+ import SwiftUI
+ 
+ struct MeetingView: View {
+ var body: some View {
+ VStack {
+ Image(systemName: "globe")
+ .imageScale(.large)
+ .foregroundColor(.accentColor)
+ Text("Hello, world!")
+ }
+ .padding()
+ }
+ }
+ 
+ struct MeetingView_Previews: PreviewProvider {
+ static var previews: some View {
+ MeetingView()
+ }
+ }
+ */
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack{
+                VStack {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                VStack {
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Davide Venier Segovia on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
